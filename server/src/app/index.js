7757ef90b48af3beb13895e8b9ca648fec3e6b07
kibuikaCodes <kibuikasteve@gmail.com>
2023-05-13 12:41:03 +0300

    :sparkles: create functional server

---
+++ server/src/app/index.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require("body-parser")
+
+// const corsOptions = require('../config/corsOptions');
+const checkOrigins = require('../middleware/checkOrigins');
+const allRoutes = require('../v1/routes/allRoutes');
+// const { redisRateLimiter } = require('../middleware/rateLimiter');
+
+
+// body parser configuration
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Handle options credentials check - before CORS!
+// and fetch cookies credentials requirement
+app.use(checkOrigins);
+
+// Cross Origin Resource Sharing
+app.use(cors({ origin: "*" }));
+
+// app.use(redisRateLimiter)
+
+// middleware for json 
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json("Hello test")
+})
+
+// health check end point
+app.get('/health', (req, res) => {
+    res.json({ status: 'API is healthy' });
+});
+
+// Function to serve all static files
+// inside images directory.
+app.use('/public/images', express.static('images'));
+
+//Api access end points
+app.use('/api/v1/kisi-test', allRoutes);
+
+app.listen(process.env.PORT, () => console.log(`Server running on ${process.env.PORT} ...`));
